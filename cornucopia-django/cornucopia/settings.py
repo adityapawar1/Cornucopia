9a910cca0103659a6c30f72d49e16b767d3807d3
Aditya Pawar <aditya.pawar@warriorlife.net>
2020-12-18 23:50:34 -0800

    taking out csrf

--- cornucopia-django/cornucopia/settings.py
+++ cornucopia-django/cornucopia/settings.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = False
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
