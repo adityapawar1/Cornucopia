8397208784c288010c3abd6f8061a44c1efa2c8f
braydentam <57848434+braydentam@users.noreply.github.com>
2020-12-20 01:12:56 -0800

    Add files via upload

--- Cornucopia/Cornucopia/upload.swift
+++ Cornucopia/Cornucopia/upload.swift
@@ -14,21 +14,68 @@ class upload: UIViewController {
     @IBOutlet weak var progress: UIProgressView!
     @IBOutlet weak var finish: UIButton!
     @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var text1: UILabel!
     @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var text2: UILabel!
     @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var text3: UILabel!
     @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var text4: UILabel!
     @IBOutlet weak var imageView5: UIImageView!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var text5: UILabel!
     var idx = 0
+    var ingredient = String()
+    
+    let model = Resnet50()
     let progressC = Progress(totalUnitCount: 10)
     override func viewDidLoad() {
         super.viewDidLoad()
+        clearButton.layer.cornerRadius = 0.5 * clearButton.bounds.size.width
         finish.layer.cornerRadius = 5.0
         finish.layer.masksToBounds = true
         progress.transform = progress.transform.scaledBy(x: 1, y: 5)
 
         // Do any additional setup after loading the view.
     }
-    
+    func imageClassification() {
+        var counter = 0
+        let imageList = [imageView1.image,imageView2.image,imageView3.image,imageView4.image,imageView5.image]
+        for i in imageList{
+            counter += 1
+        guard let img = i,
+              let resizedImage = img.resizeTo(size: CGSize(width: 224, height: 224)),
+              let buffer = resizedImage.toBuffer() else {
+            return
+        }
+            let output = try? model.prediction(image: buffer)
+
+            if let output = output {
+
+                self.ingredient = output.classLabel
+                
+
+            }
+            if counter == 1{
+                text1.text = ingredient
+            }
+            else if counter == 2{
+                text2.text = ingredient
+            }
+            else if counter == 3{
+                text3.text = ingredient
+            }
+            else if counter == 4{
+                text4.text = ingredient
+            }
+            else if counter == 5{
+                text5.text = ingredient
+            }
+
+            
+    }
+
+    }
     @IBAction func clearImages(_ sender: Any) {
         idx = 0
         imageView1.image = nil
@@ -45,9 +92,11 @@ class upload: UIViewController {
             present(ac, animated: true, completion: nil)
         }
         else {
+            self.imageClassification()
             Timer.scheduledTimer(withTimeInterval: 1, repeats: true){(timer) in
                 guard self.progressC.isFinished == false else {
                     timer.invalidate()
+                    
                     let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
                     let newViewController = storyboard.instantiateViewController(withIdentifier: "RecipePicker")
                     self.present(newViewController, animated: true, completion: nil)
@@ -208,3 +257,41 @@ extension upload: UIImagePickerControllerDelegate, UINavigationControllerDelegat
     */
 
 
+extension UIImage {
+
+    func resizeTo(size :CGSize) -> UIImage? {
+
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: size))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+
+    func toBuffer() -> CVPixelBuffer? {
+
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+        return pixelBuffer
+    }
+
+}
